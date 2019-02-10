157e30864668da7a488fd2738461899b1e5886be
unknown <nbatre1@yahoo.com>
2019-02-10 09:02:15 -0600

    keep moving forward

---
+++ foodsearch/src/tempData.js
@@ -0,0 +1,15 @@
+export const recipe ={
+  url: "http://food2fork.com/view/35382",
+  imageUrl:"http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg",
+  ingredients:[
+      "2 jalapeno peppers, cut in half lenght wise and seeded",
+      "2 slices sour dough bread",
+      "2 tablespoons cream cheese, room tempature",
+      "1/2 cup jack and cheddar cheese, shredded",
+      "1 tablespoon tortilla chips, crumbled"
+  ],
+  publisher:'Treek',
+  publisherUrl:"http://closetcooking.com",
+  sourceUrl:"http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
+  title: "Jalapeno Popper Grilled Cheese Sandwics"
+};
