2d367b4e1bad7d0a9f4c3df9c3133b0aca86714b
unknown <nbatre1@yahoo.com>
2019-02-11 18:20:05 -0600

    keep moving forward

--- foodsearch/src/components/RecipeDetails.js
+++ foodsearch/src/components/RecipeDetails.js
@@ -1,73 +1,112 @@
-import React,{Component} from 'react';
-import {recipe} from '../tempDetails';
+import React, { Component } from "react";
+import { recipe } from "../tempDetails";
+export default class RecipeDetails extends Component {
+  // constructor(props) {
+  //   super(props);
 
-class RecipeDetails extends Component{
-    constructor(props){
-        super(props)
-
-        this.state ={
-            recipe:recipe,
-            url:`https://www.food2fork.com/api/get?key=983abcc4a754078ed066d108ce6d0b21&rId=${this.props.id}`
-        }
+  //   this.state = {
+  //     recipe: recipe,
+  //     url: `https://www.food2fork.com/api/get?key=b6fb5d86fb9419eeec1129a9e77a1ada&rId=${
+  //       this.props.id
+  //     }`
+  //   };
+  // }
+  // async componentDidMount() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+  //     this.setState({
+  //       recipe: jsonData.recipe
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  state = {
+    recipe: {}
+  };
+  async componentDidMount() {
+    const id = this.props.id;
+    const url = `https://www.food2fork.com/api/get?key=983abcc4a754078ed066d108ce6d0b21&rId=${id}`;
+    try {
+      const data = await fetch(url);
+      const jsonData = await data.json();
+      this.setState(
+        (state, props) => {
+          return { recipe: jsonData.recipe };
+        },
+        () => {}
+      );
+    } catch (error) {
+      console.log(error);
     }
-  
+  }
 
- /* async componentDidMount(){
-
-    try{
-        const data = await fetch(this.state.url);
-        const jsonData = await data.json();
-    
-    
-        this.setState({recipe:jsonData.recipe})
-        }catch(error){
-          console.log(error);
-        }
-  
-   }
-*/
-
-    render(){
-         console.log(this.state.recipe);
-        const{image_url,ingredients,publisher,publisher_url,title,source_url,url} = this.state.recipe
-
-        return( 
-            <React.Fragment>
-              <div className ="container">
-              <div className ="row">
-              <div className ="col-10 mx-auto col-md-6 my-3">
-              <button type="button" className="btn-warning mb-5 text-capitalize rounded">
-               Back To List
-              </button>
-              <img src={image_url} className="d-block w-10" alt="food" />
+  render() {
+    const {
+      image_url,
+      publisher,
+      publisher_url,
+      source_url,
+      title,
+      ingredients
+    } = this.state.recipe;
+    const { handleIndex } = this.props;
+    if (!ingredients) {
+      return <h1>loading ....</h1>;
+    }
+    if (ingredients) {
+      return (
+        <React.Fragment>
+          <div className="container">
+            <div className="row">
+              <div className="col-10 mx-auto col-md-6 my-3">
+                <button
+                  type="button"
+                  className="btn btn-warning mb-5 text-capitalize"
+                  onClick={() => handleIndex(1)}
+                >
+                  back to recipe list
+                </button>
+                <img src={image_url} className="d-block w-100" alt="recipe" />
               </div>
-              {/*details section */}
-               <div className ="col-10 mx-auto col-md-6 my-3">
-                 <h6 className="text-uppercase">{title}</h6>
-                 <h6 className ="text-warning text-capitalize text-slanted">
-                  Provided to you by: {publisher}    </h6>
-                  
-                  <a href={publisher_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary mt-2 text-capitalize">Publisher Webpage</a>
-                  
-                  <a href={source_url} target="_blank" rel="noopener noreferrer" className="btn btn-success mt-2 text-capitalize mx-3">Source url</a>
-               
-                  <ul className="list-group mt-4">
-                <h2 className="mt-3 mb-4">ingredients</h2>
-                {
-                    ingredients.map((item,index)=>{
-                      return (<li className="list-group-item text-slanted" key={index}>{item}</li>)
-                    })
-                }
-
+              {/* details */}
+              <div className="col-10 mx-auto col-md-6 my-3">
+                <h6 className="text-uppercase">{title}</h6>
+                <h6 className="text-warning text-capitalize text-slanted">
+                  provided by {publisher}
+                </h6>
+                <a
+                  href={publisher_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-primary mt-2 text-capitalize"
+                >
+                  publisher webpage
+                </a>
+                <a
+                  href={source_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-success mt-2 mx-3 text-capitalize"
+                >
+                  recipe url
+                </a>
+                <ul className="list-group mt-4">
+                  <h2 className="mt-3 mb-4">Ingredients</h2>
+                  {ingredients.map((item, index) => {
+                    return (
+                      <li key={index} className="list-group-item text-slanted">
+                        {item}
+                      </li>
+                    );
+                  })}
                 </ul>
-                </div>
-               
-                </div>
               </div>
-            </React.Fragment>
-        )
+            </div>
+          </div>
+        </React.Fragment>
+      );
     }
-
+  }
 }
-
-export default RecipeDetails
--- foodsearch/src/components/RecipeList.js
+++ foodsearch/src/components/RecipeList.js
@@ -1,39 +1,48 @@
-import React,{Component} from 'react';
-import Recipe from './Recipe';
-import RecipeSearch from './RecipeSearch';
-class RecipeDetails extends Component {
+import React, { Component } from "react";
+import Recipe from "./Recipe";
+import RecipeSearch from "./RecipeSearch";
 
+export default class RecipeList extends Component {
+  render() {
+    const {
+      recipes,
+      handleDetails,
+      value,
+      handleSubmit,
+      handleChange,
+      error
+    } = this.props;
 
-    render(){
-      const{recipes} = this.props;
-
-        return(<React.Fragment>
-          <RecipeSearch />
-          <div className = "container my-5">
-          {/*title*/}
-          <div className ="row">
-          <div className ="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
-           <h1 className="text-slanted">Recipe List</h1>
-          </div>
-          </div>
-          {/*end of title */}
+    return (
+      <React.Fragment>
+        <RecipeSearch
+          value={value}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+        <div className="container my-5">
+          {/* title */}
           <div className="row">
-          {
-            recipes.map(recipe =>{
-
-          return(<Recipe key={recipe.recipe_id} recipe={recipe}>
-
-          </Recipe>
-          )
-            })
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* end of title */}
+          <div className="row">
+       {
+              recipes.map(recipe => {
+                return (
+                  <Recipe
+                    key={recipe.recipe_id}
+                    recipe={recipe}
+                    handleDetails={handleDetails}
+                  />
+                );
+              })
           }
           </div>
-          </div>
-          
-        </React.Fragment>
-        )
-    }
+        </div>
+      </React.Fragment>
+    );
+  }
 }
-
-
-export default RecipeDetails ;
